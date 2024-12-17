99c3b8e7d5a89b574c9e86d8c39f5260959afadb
OGSesetti <91277863+OGSesetti@users.noreply.github.com>
2024-12-17 20:52:41 +0200

    laptop

--- ses/game_plugins/resource_save_manager.gd
+++ ses/game_plugins/resource_save_manager.gd
@@ -4,8 +4,9 @@ extends Resource
 const local_folder_path = SesConfig.save_folder_path
 const user_folder_path = SesConfig.user_folder_path
 const auto_save_name : String = "autosave.tres"
-const manual_save_path : String = SesConfig.save_folder_path
+const manual_save_name : String = SesConfig.save_folder_path
 const enable_user_directory = SesConfig.enable_user_directory
+const manual_save_name : String = SesConfig.manual_save_name
 
 var save_folder : String
 var save_1 : String
@@ -16,13 +17,13 @@ var save_3 : String
 @export var current_level = ""
 @export var inventory = Resource
 @export var common_variables = {}
-#jee
+
 static func load_save_game():
 	pass
 	
 static func write_save_game(auto_save : bool = false):
-	if enable_user_directory == true:
-		var save_path = user_folder_path + "save"
+	var time_stamp = Time.get_date_string_from_system()
+	var save_path = user_folder_path
 	#ResourceSaver.save()
 	pass
 	
--- ses/ses_config.gd
+++ ses/ses_config.gd
@@ -1,6 +1,7 @@
+#vaihda tää paska jsoniin
+
 const enable_save_manager : bool = true
 const enable_game_settings : bool = true
-
 
 
 
@@ -11,5 +12,6 @@ const enable_game_settings : bool = true
 const save_folder_path : String = "res://ses/save_files/" #Alternative: "user://"
 const user_folder_path : String = "user://"
 const enable_user_directory : bool = false
+const manual_save_name : String = "Save" #time stamp and filetype are added by save_manager
 
 ################################################################################
