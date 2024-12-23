0c28ecaa2f42293749069f051873d77c02d2a574
OGSesetti <severi.tahtinen2@gmail.com>
2024-12-23 14:31:08 +0200

    pause menu concept

--- menu_manager/loading_icon.gd
+++ menu_manager/loading_icon.gd
@@ -4,6 +4,6 @@ var r_speed = 90
 
 func _process(delta):
 	rotation_degrees += r_speed * delta
-	print(rotation_degrees)
+	#print(rotation_degrees)
 	if rotation_degrees > 360:
 		rotation_degrees = wrapf(rotation_degrees, 0, 360)
